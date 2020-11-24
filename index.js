03b8fd15fcda9979cc83f5c00e5b1f619abc136a
Angelica <angielopez4004@gmail.com>
2020-11-23 19:09:30 -0500

    add new var in js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const $listTask = document.querySelector(".js-item");
 const $input = document.querySelector(".js-input");
 const $list = document.querySelector(".js-list");
 const $clear = document.querySelector(".js-clear");
+const $x = document.querySelector(".js-x");
+const $contentListItem = document.querySelector(".js-contentList");
 
 $buttonAdd.addEventListener("click", function() {
     $listTask.innerHTML = $input.value;
@@ -14,4 +16,7 @@ $clear.addEventListener("click", function() {
     var display = document.querySelector(".js-list").style.display= "none";
 })
 
+$x.addEventListener("click", function() {
+    $contentListItem.style.display= "none";
+})
 
