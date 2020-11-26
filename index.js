aa1e8f9c28dc276851e05fff8e50ed9b8f650ab8
Angelica <angielopez4004@gmail.com>
2020-11-26 12:30:56 -0500

    add conditional in buttonAdd

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const $buttonAdd = document.querySelector(".js-add");
-const $listTask = document.querySelector(".js-item");
+const $listTask = document.querySelector(".js-item"); // Este es el item que lleva la tarea
 const $input = document.querySelector(".js-input");
 const $list = document.querySelector(".js-list");
 const $clear = document.querySelector(".js-clear");
 const $x = document.querySelector(".js-x");
 const $contentListItem = document.querySelector(".js-contentList");
+let array = []
 
 $buttonAdd.addEventListener("click", function() {
-    $listTask.innerHTML = $input.value;
-    $input.value = "";
-    var display = document.querySelector(".js-list").style.display= "flex";
+    if(!($input.value === "")) {
+        $listTask.innerHTML = $input.value;
+        $input.value = "";
+        var display = document.querySelector(".js-list").style.display= "flex";
+    }
 })
 
 $clear.addEventListener("click", function() {
